557f764fdf0b740c3c9ab93cdfd2426e3740eedc
Ana Matei <anamatei.099@gmail.com>
2024-03-21 11:21:52 +0100

    Added reusable component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,12 @@
-import Image from "next/image";
-import Link from "next/link";
+import homeImg from "../../public/home.jpg";
+import Hero from "@/components/hero";
 
 export default function Home() {
   return (
-    <div>
-      <div>
-        <Link href="/performance">Performance</Link>
-        <Link href="/reliability">Reliability</Link>
-        <Link href="/scale">Scale</Link>
-      </div>
-      Home
-    </div>
+    <Hero
+      imgData={homeImg}
+      imgAlt="car factory"
+      title="Professional Cloud Hosting"
+    />
   );
 }
